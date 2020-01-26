f2f7d0ccd8ac6e2d259d1415a5941d210adb30ee
liguangyi <252438823@qq.com>
2020-01-26 20:25:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {HotTable} from '@handsontable/react';
+import Skeleton from 'react-loading-skeleton';
 import './App.css';
 
 function App() {
+  const [data, setState] = useState([]);
+  useEffect(() => {
+    const longRequest = fetch(`http://httpstat.us/200?sleep=${1000 * 5}`);
+    const shortRequest = fetch('https://randomuser.me/api/').then(response => response.json());
+
+    Promise.all([longRequest, shortRequest]).then(([longRequestResponse, shortRequestResponse]) => {
+      console.log(longRequestResponse, shortRequestResponse);
+      setState([
+        ['', 'Tesla', 'Mercedes', 'Toyota', 'Volvo'],
+        ['2019', 10, 11, 12, 13],
+        ['2020', 20, 11, 14, 13],
+        ['2021', 30, 15, 12, 13]
+      ])
+    })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && data.length ? <HotTable data={data} colHeaders={true} rowHeaders={true} width="600" height="300"/> :
+        <Skeleton count={15}/>}
     </div>
-  );
+  )
 }
 
 export default App;
